065cfe66e59554359c277d46025f0f5e66864bb6
Cyril-lamirand <contact.cyril1@gmail.com>
2021-02-23 16:17:12 +0100

    [ADD] JWT Private & Public

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,22 +3,48 @@
 namespace App\DataFixtures;
 
 use App\Entity\Task;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var PasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * AppFixtures constructor.
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
         for ($i = 1; $i <= 10; $i++) {
-            $task = new Task();
-            $task->setName('Tache '.$i);
-            $task->setDescription('Description de la tache '.$i);
-            $task->setDone(0);
+            $user = new User();
+            $user->setEmail('User'.$i.'@gmail.com');
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'password'));
+            $manager->persist($user);
+        }
+        $manager->flush();
 
+        for ($i = 1; $i <= 10; $i++) {
+            $task = new Task();
+            $task->setName('Tâche '.$i);
+            $task->setDescription('Description blabla '.$i);
+            $task->setDone(0);
             $manager->persist($task);
         }
-
         $manager->flush();
     }
 }
+
